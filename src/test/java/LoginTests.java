d7d33ed8919da8f9f7decf0f8d0fd0fc334954ac
Stanislav Telyakaev <stelyakaev@usetech.ru>
2023-09-05 12:26:44 +0300

    test

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class LoginTests {
+    @Test
+    void successfulLoginTest() {
+        Configuration.holdBrowserOpen = true;
+
+
+
+        open("https://qa.guru/cms/system/login");
+        $(".login-form").shouldHave(text("Войти"));
+        $("[name=email]").setValue("stelyakaev@rtc.usetech.ru");
+        $("[name=password]").setValue("RleBchx!3LgXu");
+        $(".btn-success").click();
+        $(".main-header__login").click();
+//        open("https://qa.guru/cms/system/login");
+
+        $(".logined-form").shouldHave(text("Здравствуйте, Станислав"));
+
+
+
+
+    }
+}
